9bc2b342f4db9d314d1fbf873758dfb7ea28c431
Piotr Starzec <starzec.piotr.12@gmail.com>
2020-09-23 19:31:33 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pbt-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'peachtree-bank-test';
+}
