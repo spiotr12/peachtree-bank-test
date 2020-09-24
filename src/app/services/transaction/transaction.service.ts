e6fd90de2eb2ae362965246bf8ce5073c098987b
Piotr Starzec <starzec.piotr.12@gmail.com>
2020-09-24 17:31:28 +0200

    fix minors

--- src/app/services/transaction/transaction.service.ts
+++ src/app/services/transaction/transaction.service.ts
@@ -1,18 +1,29 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { ITransactionRecord } from 'src/app/models';
-import { MockTransactionService } from './mock-transaction.service';
+import * as transactions from 'src/mock/transactions.json';
+import { map } from 'rxjs/operators';
 
 
 @Injectable({
   providedIn: 'root',
-  useClass: MockTransactionService,
 })
 export class TransactionService {
 
   constructor() { }
 
   public getTransactions(): Observable<ITransactionRecord[]> {
-    return of([]); // This would call rest api server
+    return of([...transactions.data] as any[]).pipe(
+      map((trans) => trans
+        .map((t) => {
+          // Map transaction date to Date
+          t.dates.valueDate = new Date(t.dates.valueDate);
+          // Map amount to number
+          const amountMod = t.transaction.creditDebitIndicator === 'DBIT' ? -1 : 1;
+          t.transaction.amountCurrency.amount = amountMod * Number(t.transaction.amountCurrency.amount);
+          return t;
+        }),
+      ),
+    );
   }
 }
