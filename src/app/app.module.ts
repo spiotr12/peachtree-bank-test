38fe658bcfdd56d85fbe9ce99314ae5b9d91bfe6
Piotr Starzec <starzec.piotr.12@gmail.com>
2020-09-25 00:11:31 +0200

    fix app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { environment } from 'src/environments/environment';
-import { AppEffects, REDUCERS_TOKEN } from './+state';
+import { AppEffects, reducerProvider, REDUCERS_TOKEN } from './+state';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -26,7 +26,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
       logOnly: environment.production,
     }),
   ],
-  providers: [],
+  providers: [reducerProvider],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
