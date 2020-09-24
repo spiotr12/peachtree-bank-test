9883118047295c4f26ec6670b2ffc9fc8316b01e
Piotr Starzec <starzec.piotr.12@gmail.com>
2020-09-24 08:27:28 +0200

    fix responsiveness and and transactions-history

--- src/app/test-page/test-page.module.ts
+++ src/app/test-page/test-page.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 import { TestPageComponent } from './test-page.component';
 import { TestPageRoutingModule } from './test-page-routing.module';
 import { PanelModule } from 'src/app/components';
+import { TransactionsHistoryComponent } from './transactions-history';
+import { TransferFormComponent } from './transfer-form';
 
 
 @NgModule({
   declarations: [
     TestPageComponent,
+    TransactionsHistoryComponent,
+    TransferFormComponent,
   ],
   imports: [
     CommonModule,
---
+++ src/app/test-page/transactions-history/transactions-history.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component, HostBinding } from '@angular/core';
+
+
+@Component({
+  selector: 'pbt-transactions-history',
+  templateUrl: './transactions-history.component.html',
+  styleUrls: ['./transactions-history.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TransactionsHistoryComponent {
+
+  @HostBinding('class.pbt-transactions-history')
+  private readonly mainCSSClass = true;
+
+  constructor() { }
+}
